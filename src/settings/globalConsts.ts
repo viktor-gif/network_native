8ecbc37e22a03592d741811c16544fca5651d3cc
viktor-gif <klim.victor.ua@gmail.com>
2023-04-21 21:51:35 +0300

    Added img for deleted accaunt and added one more ip-address in settings

--- src/settings/globalConsts.ts
+++ src/settings/globalConsts.ts
@@ -1,5 +1,8 @@
 // for Budkiv, babushka
-export const WIFI_API = "192.168.1.7"
+// export const WIFI_API = "192.168.1.7"
 
-// for BIvanaBoguna
+// for IvanaBoguna
 // export const WIFI_API = "192.168.1.11"
+
+// for Vugly
+export const WIFI_API = "192.168.0.11"
