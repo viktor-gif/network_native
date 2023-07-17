a799db7b9c3eb7501d6047f945d571a7e4d1a871
viktor-gif <klim.victor.ua@gmail.com>
2023-07-17 16:48:59 +0300

    follow and unfollow users

--- src/components/users/user/user.tsx
+++ src/components/users/user/user.tsx
@@ -1,5 +1,7 @@
 import { useNavigation } from "@react-navigation/native"
+import { useEffect, useState } from "react"
 import { StyleSheet, View, Text, Image, Button, TouchableOpacity } from "react-native"
+import { followAPI } from "../../../api/follow"
 import { LocationType, PhotosType } from "../../../ts/users"
 import { getCorrectMediaUrl } from "../../../utils/commonFunctions"
 
@@ -14,12 +16,54 @@ type PropsType = {
 }
 
 export const User = (props: PropsType) => {
+    const [userFollow_status, setUserFollow_status] = useState("")
+    // const [followButtonText, setFollowButtonText] = useState("Додати")
+    const getFollow = () => {
+        followAPI.getFollow(props.id).then(res => {
+            if (res.data != userFollow_status) setUserFollow_status(res.data)
+        })
+    }
+    useEffect(() => {
+        getFollow()
+    }, [])
+
+    // if (userFollow_status === "") {
+    //     setFollowButtonText("Додати до друзів")
+    // } else if (userFollow_status === "pending-for-answer") {
+    //     setFollowButtonText("Відмінити заявку")
+    // } else if (userFollow_status === "query-for-answer") {
+    //     setFollowButtonText("Прийняти заявку")
+    // } else if (userFollow_status === "followed") {
+    //     setFollowButtonText("Видалити з друзів")
+    // }
+    let followButtonText = ""
+
+    if (userFollow_status === "") {
+        followButtonText = "Додати до друзів"
+    } else if (userFollow_status === "pending-for-answer") {
+        followButtonText = "Відмінити заявку"
+    } else if (userFollow_status === "query-for-answer") {
+        followButtonText = "Прийняти заявку"
+    } else if (userFollow_status === "followed") {
+        followButtonText = "Видалити з друзів"
+    }
+
     const photoUrl = props.photos.small
 
     const navigation: any = useNavigation()
 
     const navigateTo = (path: string, userId: string) => {
         navigation.navigate(path, {userId})
+    }
+
+    const followPress = () => {
+        if (userFollow_status === "" || userFollow_status === "query-for-answer") {
+            followAPI.setFollow(props.id).then(() => getFollow()) 
+            console.log(props.id)
+        } else {
+            followAPI.deleteFollow(props.id).then(() => getFollow()) 
+            console.log(props.id)
+        }
     }
     
     return <View style={styles.container}>
@@ -42,10 +86,12 @@ export const User = (props: PropsType) => {
             <View>
                 <Text>{props.fullName}</Text>
             </View>
-            <View style={styles.buttonsBlock}>
-                <Button title='Додати' />
-                <Button title='Видалити' />
-            </View>
+            <TouchableOpacity
+                style={styles.buttonsBlock}
+                onPress={followPress}
+            >
+                <Text style={styles.buttonsBlock_text}>{followButtonText}</Text>
+            </TouchableOpacity>
         </View>
     </View>
 }
@@ -61,8 +107,11 @@ const styles = StyleSheet.create({
         gap: 20
     },
     buttonsBlock: {
-        width: '100%',
-        flexDirection: 'row',
+        
+    },
+    buttonsBlock_text: {
+        color: 'blue',
+        fontSize: 20
     },
     ava: {
         width: 100,
